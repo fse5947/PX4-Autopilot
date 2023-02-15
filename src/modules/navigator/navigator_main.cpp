b40bbd008d7602579cb74e04cff65f18fe628974
Ryan <ryan.sheppard@shearwater.ai>
2023-02-15 13:48:45 -0500

    fixed variable type bug in cmd_param_changee

--- src/modules/navigator/navigator_main.cpp
+++ src/modules/navigator/navigator_main.cpp
@@ -510,33 +510,32 @@ void Navigator::run()
 				publish_vehicle_command_ack(cmd, vehicle_command_s::VEHICLE_CMD_RESULT_ACCEPTED);
 
 			} else if(cmd.command == vehicle_command_s::VEHICLE_CMD_DO_SET_PARAMETER){
-				// create the change, just like mavlink cmd param_set
 				PX4_WARN("SET PARAMETER Mission Item");
 				param_t param = param_for_index(cmd.param1);
-				int old_value = 0;
-				int new_value = cmd.param2;
-				// param_t param_old = param_get(param, &old_value);
-				param_get(param, &old_value);
 				if (param == PARAM_INVALID) {
 					PX4_ERR("unknown param: %i", (int)cmd.param1);
-
-				//! Mavlink warning to be implemented
-				/*}else if (!((param_type(param) == PARAM_TYPE_INT32 && cmd.param2 == PARAM_TYPE_INT32) ||
-					     (param_type(param) == PARAM_TYPE_FLOAT && cmd.param2 == PARAM_TYPE_FLOAT))) {
-					PX4_ERR("param types mismatch param: %i", cmd.param1);
-				*/
 				} else {
-					// According to the mavlink spec we should always acknowledge a write operation.
-					param_set(param, &new_value);
+					switch (param_type(param)) {
+					case PARAM_TYPE_INT32: {
+						int32_t new_value = cmd.param2;
+						int32_t old_value = 0;
+						param_get(param, &old_value);
+						param_set(param, &new_value);
+						PX4_WARN("parameter: %s changed from %f to %f", param_name(param), (double)old_value, (double)cmd.param2);
+						}
+						break;
+
+					case PARAM_TYPE_FLOAT: {
+						float new_value = cmd.param2;
+						float old_value = 0;
+						param_get(param, &old_value);
+						param_set(param, &new_value);
+						PX4_WARN("parameter: %s changed from %f to %f", param_name(param), (double)old_value, (double)cmd.param2);
+						}
+						break;
+					}
 				}
 
-				int check_save = 0;
-				param_get(param, &check_save);
-				if (abs(check_save - (float)new_value) > 0.0001f){
-					PX4_ERR("Error: param value was not changed");
-				} else{
-					PX4_WARN("parameter: %s changed from %f to %f", param_name(param), (double)old_value, (double)cmd.param2);
-				}
 
 			} else if (cmd.command == vehicle_command_s::VEHICLE_CMD_DO_SET_ROI
 				   || cmd.command == vehicle_command_s::VEHICLE_CMD_NAV_ROI
